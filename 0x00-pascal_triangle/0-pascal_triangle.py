3f6452ed37452cfba9db68cd43b1669e8af02a84
saaddighi <saaddighi20@gmail.com>
2024-11-19 10:35:34 +0000

    ok

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,12 +3,13 @@
 triangle = [[1]]
 
 def pascal_triangle(n):
-    if n <= 0:
+    # a function where we can put how many rows we want.
+    if n <= 0: # returns an empty list if n <= 0.
         return []
 
-    for i in range(1,n):
+    for i in range(1,n): # first loop to itereat over the number of rows
         row = [1]
-        for j in range(1,i):
+        for j in range(1,i): # nested loop to itereat over elements of each row
             row.append(tirangle[i-1][-j] + tirangle[i-1][j])
         row.append(1)
         triangle.append(row)
