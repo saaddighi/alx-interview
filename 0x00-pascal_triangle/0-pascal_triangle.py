6144b2ff1dced5af58761c86f5e0d4fded0386fd
saaddighi <saaddighi20@gmail.com>
2024-11-19 10:32:07 +0000

    ok

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,15 @@
+#!/usr/bin/python3
+
+triangle = [[1]]
+
 def pascal_triangle(n):
     if n <= 0:
-       return []
-    triangle = [[1]]
+        return []
+
     for i in range(1,n):
         row = [1]
         for j in range(1,i):
-            row.append(triangle[i-1][-j] + triangle[i-1][j])
-            print(row)
+            row.append(tirangle[i-1][-j] + tirangle[i-1][j])
         row.append(1)
-        print(row)
         triangle.append(row)
-    return triangle
-
-print(pascal_triangle(2))
-       
+    return tirangle
