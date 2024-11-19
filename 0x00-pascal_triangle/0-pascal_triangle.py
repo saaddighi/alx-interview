7404d8b570b73b8c9a2d8b9333aa060954e71827
saaddighi <saaddighi20@gmail.com>
2024-11-19 10:39:43 +0000

    ok

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 
-import random
-""" random module
-
-    Returns:
-    module: help us generate random things
-"""
 triangle = [[1]]
 
 def pascal_triangle(n):
-    # """ a function where we can put how many rows we want."""
-    if n <= 0: # """ returns an empty list if n <= 0"""
+    """ a function where we can put how many rows we want."""
+    if n <= 0:  
+        """ returns an empty list if n <= 0"""
         return []
 
     for i in range(1,n): # first loop to itereat over the number of rows
