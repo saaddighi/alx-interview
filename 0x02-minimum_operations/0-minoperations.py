90a139bce184fe8c868df44de3f5697e9d5d12a5
saaddighi <saaddighi20@gmail.com>
2024-11-26 09:30:58 +0000

    ok

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,13 +2,14 @@
 """a typical interview question or test that is asked
     """
 
+
 def minOperations(n) -> int:
     """a function that takes a number(n) and returns the number of operations
-    needed to have H n number of times in a file using only the copy all and 
+    needed to have H n number of times in a file using only the copy all and
     paste operations"""
     if n <= 1:
         return 0
-    
+
     op = 0
     for i in range(2, n + 1):
         while n % i == 0:
@@ -18,4 +19,3 @@ def minOperations(n) -> int:
         return op
     else:
         return 0
-
