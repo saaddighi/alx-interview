42f06ebb7d743fa128608c17bc7cbd82db5ae9a6
saaddighi <saaddighi20@gmail.com>
2024-11-26 09:27:16 +0000

    ok

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+
 """a typical interview question or test that is asked
     """
 
@@ -6,33 +6,16 @@ def minOperations(n) -> int:
     """a function that takes a number(n) and returns the number of operations
     needed to have H n number of times in a file using only the copy all and 
     paste operations"""
-    op = 0
-    if n % 2 == 0:
-        with open('test.txt', 'r') as file:
-    
-            char = file.read()
-            ch = char*2
-        op = 1 + (n // 2)
-        for i in range((n // 2)):
-            with open ('tst.txt','a') as d_file:
-                d_file.write(ch)
-        with open('tst.txt', 'r') as d_file:
-            res = d_file.read()
-        return op 
-    elif n % 3 == 0:
-        with open('test.txt', 'r') as file:
-            
-            char = file.read()
-            ch = char*3
-            op = 3 + (n // 3)
-        for i in range((n // 3)):
-            with open ('tst.txt','a') as d_file:
-                d_file.write(ch)
-        with open('tst.txt', 'r') as d_file:
-            res = d_file.read()
-        return op
-    elif n <= 1:
+    if n <= 1:
         return 0
+    
+    op = 0
+    for i in range(2, n + 1):
+        while n % i == 0:
+            n //= i
+            op += i
+    if n == 1:
+        return op
     else:
         return 0
 
